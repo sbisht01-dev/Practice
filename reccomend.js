2ae31961a79f3a43a8f01f19e2c165eb802c4e33
Saurabh Bisht <76562057+sbisht01-dev@users.noreply.github.com>
2022-10-01 12:33:22 +0530

    Update reccomend.js

--- reccomend.js
+++ reccomend.js
@@ -3,33 +3,47 @@ Recomendation system
 */
 
 /* Array Declared*/
-let batch = ["A", "B", "C", "D", "E"];
+let batch = ['A','B','C','D','E']
 
 /* function for selection of any alphabet*/
-function selection() {
-  console.log("1-A , 1-B , 3-C , 4-D , 5-E");
-  console.log("Select your fav alphabet");
+function selection(){
+console.log("1-A , 1-B , 3-C , 4-D , 5-E")
+console.log("Select your fav alphabet")
   /* flag array*/
-  let flag = [0, 0, 0, 0, 0];
-  for (let i = 0; i < flag.length; i++) {
-    let choice = prompt("Enter");
-    if (choice > 0 && choice <= 5) {
-      flag[choice - 1] = flag[choice - 1] + 1;
-      console.log(flag);
-    } else {
-      console.log("Enter valid number.");
+  
+let flag = [0,0,0,0,0]
+for(let i=0; i<flag.length; i++ ){
+  let choice = prompt("Enter")
+if(choice>0 && choice<=5){
+flag[choice-1] = flag[choice-1] + 1
+console.log(flag)
+  }else{
+    console.log("Enter valid number.")
     }
   }
-  return flag;
+  console.log("New Flag \n")
+  return flag
 }
-/* more loops can be used for the automation
 
-/*generating random values for system*/
-function gen() {
-  let store = [0, 0, 0, 0, 0];
-  for (let i = 0; i < flag.length; i++) {
-    let x = Math.random();
-    store[i] = store[i] + x;
+let flag = selection()
+console.log("New Flag is:" + flag)
+
+let final = []
+  function values(){
+  let y = 0
+  for(let i=0;i<flag.length;i++){                       /*index of flag*/
+    for(let j=1;j<=flag[i];j++){                        /*value at that index*/
+      if(flag[i]>=1){
+      y = y + Math.random()
+      }else{y= y - 0.05}
+    }
+    final[i] = y.toFixed(5)
+    y=0
+    /*console.log(final)*/
   }
-  console.log(store);
+  return final
 }
+
+let reccomend = values()
+
+console.log(reccomend)
